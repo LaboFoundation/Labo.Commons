77339384aef4fa5e945c317dea76270b1dceb20d
Bora Akgun <boraakgn@gmail.com>
2013-08-30 22:14:50 +0300

    added objectutils tests

---
+++ Labo.Common.Test/Utils/ObjectUtilsTestFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Labo.Common.Utils;
+using NUnit.Framework;
+
+namespace Labo.Common.Tests.Utils
+{
+    [TestFixture]
+    public class ObjectUtilsTestFixture
+    {
+        [Test]
+        public void IsNull()
+        {
+            string s = null;
+            Assert.AreEqual("aaa", ObjectUtils.IsNull(s, () => "aaa"));
+            Assert.AreEqual("bbb", ObjectUtils.IsNull("bbb", () => "aaa"));
+        }
+
+        [Test]
+        public void IsNullThrowsException()
+        {
+            string s = null;
+            Assert.Throws<ArgumentNullException>(() => ObjectUtils.IsNull(s, null));
+        }
+
+        [Test]
+        public void Cast()
+        {
+            object s = "aaa";
+            Assert.AreEqual("aaa", ObjectUtils.Cast<string>(s));
+        }
+    }
+}
