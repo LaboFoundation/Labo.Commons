b4ad364162fc7fa9f276554828b4f9e6a69cb75a
Bora Akgun <boraakgn@gmail.com>
2013-09-06 20:15:58 +0300

    added simpleinjector project

---
+++ Labo.Common.Ioc.SimpleInjector/SimpleInjectorIocContainer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labo.Common.Ioc.SimpleInjector
+{
+    public sealed class SimpleInjectorIocContainer : BaseIocContainer
+    {
+        public override void RegisterSingleInstance<TImplementation>(Func<IIocContainerResolver, TImplementation> creator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RegisterSingleInstanceNamed<TImplementation>(Func<IIocContainerResolver, TImplementation> creator, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RegisterSingleInstance<TService, TImplementation>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RegisterSingleInstanceNamed<TService, TImplementation>(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RegisterSingleInstance(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RegisterInstance<TService, TImplementation>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RegisterInstance(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RegisterAssemblyTypes(Type type, params System.Reflection.Assembly[] assemblies)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object GetInstance(Type serviceType, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object GetInstance(Type serviceType, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object GetInstanceOptional(Type serviceType, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsRegistered(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsRegistered(Type type, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
