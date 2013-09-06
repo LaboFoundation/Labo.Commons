3076bb0104ee749857275122a9000d49c136b951
Bora Akgun <boraakgn@gmail.com>
2013-09-06 22:18:53 +0300

    added some major ioc container projects

--- Labo.Common.Ioc.SimpleInjector/SimpleInjectorIocContainer.cs
+++ Labo.Common.Ioc.SimpleInjector/SimpleInjectorIocContainer.cs
@@ -25,7 +25,6 @@
 namespace Labo.Common.Ioc.SimpleInjector
 {
     using System;
-    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
