7b1776921a9ec6706e1dd9a6355f6a07cb4dcf89
Bora Akgun <boraakgn@gmail.com>
2013-09-09 17:52:27 +0300

    added some ioc container implementations

---
+++ Labo.Common.Ioc.LinFu/LinfuIocContainer.cs
@@ -0,0 +1,299 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LinfuIocContainer.cs" company="Labo">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2013 Bora Akgun
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy of
+//   this software and associated documentation files (the "Software"), to deal in
+//   the Software without restriction, including without limitation the rights to
+//   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//   the Software, and to permit persons to whom the Software is furnished to do so,
+//   subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//   FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary>
+//   Defines the LinfuIocContainer type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Labo.Common.Ioc.Linfu
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+    using LinFu.IoC;
+    using LinFu.IoC.Configuration;
+
+    /// <summary>
+    /// Autofac inversion of control container class.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here.")]
+    public sealed class LinfuIocContainer : BaseIocContainer
+    {
+        /// <summary>
+        /// The container
+        /// </summary>
+        private readonly ServiceContainer m_Container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinfuIocContainer"/> class.
+        /// </summary>
+        public LinfuIocContainer()
+        {
+            m_Container = new ServiceContainer();
+        }
+
+        /// <summary>
+        /// Registers the single instance.
+        /// </summary>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="creator">The creator delegate.</param>
+        public override void RegisterSingleInstance<TImplementation>(Func<IIocContainerResolver, TImplementation> creator)
+        {
+            m_Container.AddService(x => creator(this), LifecycleType.Singleton);
+        }
+
+        /// <summary>
+        /// Registers the single instance named.
+        /// </summary>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="creator">The creator delegate.</param>
+        /// <param name="name">The instance name.</param>
+        public override void RegisterSingleInstanceNamed<TImplementation>(Func<IIocContainerResolver, TImplementation> creator, string name)
+        {
+            m_Container.AddService(name, x => creator(this), LifecycleType.Singleton);
+        }
+
+        /// <summary>
+        /// Registers the single instance.
+        /// </summary>
+        /// <param name="serviceType">The type of the service.</param>
+        /// <param name="implementationType">The type of the implementation.</param>
+        public override void RegisterSingleInstance(Type serviceType, Type implementationType)
+        {
+            m_Container.AddService(serviceType, implementationType, LifecycleType.Singleton);
+        }
+
+        /// <summary>
+        /// The register singleton named instance.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="implementationType">
+        /// The implementation type.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        public override void RegisterSingleInstanceNamed(Type serviceType, Type implementationType, string name)
+        {
+            m_Container.AddService(name, serviceType, implementationType);
+        }
+
+        /// <summary>
+        /// The register singleton named instance.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        public override void RegisterSingleInstanceNamed(Type serviceType, string name)
+        {
+            m_Container.AddService(name, serviceType, serviceType, LifecycleType.Singleton);
+        }
+
+        /// <summary>
+        /// Registers the instance.
+        /// </summary>
+        /// <param name="creator">
+        /// The creator.
+        /// </param>
+        /// <typeparam name="TImplementation">
+        /// The implementation type.
+        /// </typeparam>
+        public override void RegisterInstance<TImplementation>(Func<IIocContainerResolver, TImplementation> creator)
+        {
+           m_Container.AddService(x => creator(this), LifecycleType.OncePerRequest);
+        }
+
+        /// <summary>
+        /// Registers the instance.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="implementationType">
+        /// The implementation type.
+        /// </param>
+        public override void RegisterInstance(Type serviceType, Type implementationType)
+        {
+            m_Container.AddService(serviceType, implementationType, LifecycleType.OncePerRequest);
+        }
+
+        /// <summary>
+        /// Registers the instance named.
+        /// </summary>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="creator">The creator delegate.</param>
+        /// <param name="name">The instance name.</param>
+        public override void RegisterInstanceNamed<TImplementation>(Func<IIocContainerResolver, TImplementation> creator, string name)
+        {
+            m_Container.AddService(name, x => creator(this), LifecycleType.OncePerRequest);
+        }
+
+        /// <summary>
+        /// The register named instance.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="implementationType">
+        /// The implementation type.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        public override void RegisterInstanceNamed(Type serviceType, Type implementationType, string name)
+        {
+            m_Container.AddService(name, serviceType, implementationType, LifecycleType.OncePerThread);
+        }
+
+        /// <summary>
+        /// The register ınstance named.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        public override void RegisterInstanceNamed(Type serviceType, string name)
+        {
+            m_Container.AddService(name, serviceType, serviceType, LifecycleType.OncePerThread);
+        }
+
+        /// <summary>
+        /// Registers the single instance.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        public override void RegisterSingleInstance(Type serviceType)
+        {
+            m_Container.AddService(serviceType, serviceType, LifecycleType.Singleton);
+        }
+
+        /// <summary>
+        /// Registers the instance.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        public override void RegisterInstance(Type serviceType)
+        {
+            m_Container.AddService(serviceType, serviceType, LifecycleType.OncePerRequest);
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>instance.</returns>
+        public override object GetInstance(Type serviceType, params object[] parameters)
+        {
+            return m_Container.GetService(serviceType, parameters);
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>instance.</returns>
+        public override object GetInstanceByName(Type serviceType, string name, params object[] parameters)
+        {
+            return m_Container.GetService(name, serviceType, parameters);
+        }
+
+        /// <summary>
+        /// Gets the instance optional.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>instance.</returns>
+        public override object GetInstanceOptional(Type serviceType, params object[] parameters)
+        {
+            if (!m_Container.Contains(serviceType))
+            {
+                return null;
+            }
+
+            return m_Container.GetService(serviceType, parameters);
+        }
+
+        /// <summary>
+        /// Gets the instance optional by name.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>instance.</returns>
+        public override object GetInstanceOptionalByName(Type serviceType, string name, params object[] parameters)
+        {
+            if (!m_Container.Contains(serviceType))
+            {
+                return null;
+            }
+
+            return m_Container.GetService(name, serviceType, parameters);
+        }
+
+        /// <summary>
+        /// Gets all instances.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <returns>all instances.</returns>
+        public override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return m_Container.GetServices(x => x.ServiceType == serviceType).Select(x => x.Object);
+        }
+
+        /// <summary>
+        /// Determines whether the specified type is registered.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified type is registered; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool IsRegistered(Type type)
+        {
+            return m_Container.Contains(type);
+        }
+
+        /// <summary>
+        /// Determines whether the specified type is registered.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="name">The name.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified type is registered; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool IsRegistered(Type type, string name)
+        {
+            return m_Container.Contains(name, type);
+        }
+    }
+}
---
+++ Labo.Common.Ioc.Petite/PetiteIocContainer.cs
@@ -0,0 +1,287 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PetiteIocContainer.cs" company="Labo">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2013 Bora Akgun
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy of
+//   this software and associated documentation files (the "Software"), to deal in
+//   the Software without restriction, including without limitation the rights to
+//   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//   the Software, and to permit persons to whom the Software is furnished to do so,
+//   subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//   FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary>
+//   Defines the PetiteIocContainer type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Labo.Common.Ioc.Petite
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    using global::Petite;
+
+    /// <summary>
+    /// Autofac inversion of control container class.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here.")]
+    public sealed class PetiteIocContainer : BaseIocContainer
+    {
+        /// <summary>
+        /// The container
+        /// </summary>
+        private readonly Container m_Container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PetiteIocContainer"/> class.
+        /// </summary>
+        public PetiteIocContainer()
+        {
+            m_Container = new Container();
+        }
+
+        /// <summary>
+        /// Registers the single instance.
+        /// </summary>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="creator">The creator delegate.</param>
+        public override void RegisterSingleInstance<TImplementation>(Func<IIocContainerResolver, TImplementation> creator)
+        {
+            m_Container.RegisterSingleton(x => creator(this));
+        }
+
+        /// <summary>
+        /// Registers the single instance named.
+        /// </summary>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="creator">The creator delegate.</param>
+        /// <param name="name">The instance name.</param>
+        public override void RegisterSingleInstanceNamed<TImplementation>(Func<IIocContainerResolver, TImplementation> creator, string name)
+        {
+            m_Container.RegisterSingleton(name, x => creator(this));
+        }
+
+        /// <summary>
+        /// Registers the single instance.
+        /// </summary>
+        /// <param name="serviceType">The type of the service.</param>
+        /// <param name="implementationType">The type of the implementation.</param>
+        public override void RegisterSingleInstance(Type serviceType, Type implementationType)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The register singleton named instance.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="implementationType">
+        /// The implementation type.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        public override void RegisterSingleInstanceNamed(Type serviceType, Type implementationType, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The register singleton named instance.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        public override void RegisterSingleInstanceNamed(Type serviceType, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Registers the instance.
+        /// </summary>
+        /// <param name="creator">
+        /// The creator.
+        /// </param>
+        /// <typeparam name="TImplementation">
+        /// The implementation type.
+        /// </typeparam>
+        public override void RegisterInstance<TImplementation>(Func<IIocContainerResolver, TImplementation> creator)
+        {
+            m_Container.Register(x => creator(this));
+        }
+
+        /// <summary>
+        /// Registers the instance.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="implementationType">
+        /// The implementation type.
+        /// </param>
+        public override void RegisterInstance(Type serviceType, Type implementationType)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Registers the instance named.
+        /// </summary>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="creator">The creator delegate.</param>
+        /// <param name="name">The instance name.</param>
+        public override void RegisterInstanceNamed<TImplementation>(Func<IIocContainerResolver, TImplementation> creator, string name)
+        {
+            m_Container.Register(name, x => creator(this));
+        }
+
+        /// <summary>
+        /// The register named instance.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="implementationType">
+        /// The implementation type.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        public override void RegisterInstanceNamed(Type serviceType, Type implementationType, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The register ınstance named.
+        /// </summary>
+        /// <param name="serviceType">
+        /// The service type.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        public override void RegisterInstanceNamed(Type serviceType, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Registers the single instance.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        public override void RegisterSingleInstance(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Registers the instance.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        public override void RegisterInstance(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>instance.</returns>
+        public override object GetInstance(Type serviceType, params object[] parameters)
+        {
+            return m_Container.Resolve(serviceType);
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>instance.</returns>
+        public override object GetInstanceByName(Type serviceType, string name, params object[] parameters)
+        {
+            return m_Container.Resolve(name, serviceType);
+        }
+
+        /// <summary>
+        /// Gets the instance optional.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>instance.</returns>
+        public override object GetInstanceOptional(Type serviceType, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the instance optional by name.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>instance.</returns>
+        public override object GetInstanceOptionalByName(Type serviceType, string name, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets all instances.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <returns>all instances.</returns>
+        public override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return m_Container.ResolveAll(serviceType);
+        }
+
+        /// <summary>
+        /// Determines whether the specified type is registered.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified type is registered; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool IsRegistered(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines whether the specified type is registered.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="name">The name.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified type is registered; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool IsRegistered(Type type, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
