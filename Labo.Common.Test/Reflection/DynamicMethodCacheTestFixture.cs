b19cadb3838ac429594a13d570e796044a489b62
Bora Akgun <boraakgn@gmail.com>
2013-12-16 10:39:23 +0200

    added some tests

---
+++ Labo.Common.Test/Reflection/DynamicMethodCacheTestFixture.cs
@@ -0,0 +1,26 @@
+﻿namespace Labo.Common.Tests.Reflection
+{
+    using System;
+    using System.Reflection.Emit;
+    using Labo.Common.Reflection;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DynamicMethodCacheTestFixture
+    {
+        [Test]
+        public void Get()
+        {
+            DynamicMethodCache dynamicMethodCache = new DynamicMethodCache();
+            MethodInvoker methodInvoker = (o, parameters) => null;
+            Func<MethodInvoker> creatorFunc = () => methodInvoker;
+            MethodInvoker cachedMethodInvoker = dynamicMethodCache.GetOrAddDelegate(
+                new DynamicMethod("Test", typeof(void), Type.EmptyTypes, true),
+                creatorFunc,
+                DynamicMethodCacheStrategy.Temporary);
+
+            creatorFunc = null;
+            cachedMethodInvoker.ToStringInvariant();
+        }
+    }
+}
